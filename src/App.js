1e10aed4d77ae4def6856637625fd34e1c8c5d07
qiuyuli <qiuyuli@Ctrip.com>
2018-12-02 23:34:16 +0800

    make structure

--- src/App.js
+++ src/App.js
@@ -1,121 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-import Card from './components/Card'
 import FoldCard from './components/FoldCard'
 
 let cards = ['card1', 'card2', 'card3', 'card4', 'card5', 'card6']
-let cardHeight = 80
-let cardMargin = 20
-let top = (cardHeight + cardMargin)/2
-let DISTANCE = 100
+
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      status: true,
-      touchStartY: 0,
-      touchMoveY: 0,
-      touchEndY: 0,
-    }
-  }
-  componentDidMount() {
-    console.info('componentDidMount')
-    document.addEventListener('touchstart', this.touchstart)
-    document.addEventListener('touchmove', this.touchmove)
-    document.addEventListener('touchend', this.touchend)
-  }
-  touchstart = (e) => {
-    this.setState({
-      touchStartY: e.targetTouches[0].pageY
-    })
-  }
-  touchmove = (e) => {
-    const touchMoveY = e.changedTouches[0].pageY - this.state.touchStartY
-    const {status} = this.state
-    if ((status && touchMoveY > 0) || (!status && touchMoveY < 0)) {
-      return
-    }
-    if (touchMoveY <= DISTANCE && touchMoveY >= -DISTANCE) {
-      this.setState({
-        touchMoveY: touchMoveY.toFixed(),
-      })
-    } else if(touchMoveY > DISTANCE) {
-      this.setState({
-        touchMoveY: DISTANCE
-      })
-    } else if (touchMoveY < -DISTANCE) {
-      this.setState({
-        touchMoveY: -DISTANCE
-      })
-    }
-  }
-  touchend = (e) => {
-    const touchMoveY = e.changedTouches[0].pageY - this.state.touchStartY
-    const {status} = this.state
-    if (touchMoveY > 0 && touchMoveY < DISTANCE) { // 恢复
-      console.info('huifu', touchMoveY)
-      this.setState({
-        touchMoveY: status ? 100 : 0,
-      })
-    } else if (touchMoveY >= DISTANCE) { // 展开
-      console.info('zhankai', touchMoveY)
-      this.setState({
-        touchMoveY: 100,
-        status: true
-      })
-    } else if (touchMoveY > -DISTANCE && touchMoveY < 0) { // 恢复
-      console.info('huifu', touchMoveY)
-      this.setState({
-        touchMoveY: status ? 100 : 0,
-        
-      })
-    } else if (touchMoveY <= -DISTANCE) { // 收起
-      console.info('shouqi', touchMoveY)
-      this.setState({
-        touchMoveY: 0,
-        status: false,
-      })
-    }
-  }
-  componentWillUnmount() {
-    document.removeEventListener('touchstart', this.touchstart)
-    document.removeEventListener('touchmove', this.touchmove)
-    document.removeEventListener('touchend', this.touchend)
-    console.info('componentWillUnmount')
-  }
+
   render() {
-    const {touchMoveY} = this.state
     return (
       <div className="container">
         <FoldCard>
           {
-            cards.map(card => {
+            cards.map((card, index) => {
+              //这里面可以放 每个卡片的内容
               return (
-                <div>{card}</div>
+                <div className="item" key={index}>{card}</div>
               )
             })
           }
         </FoldCard>
-        {
-          cards.map((card, index) => {
-            const style={
-              top: -index*(top - touchMoveY*0.5),
-              height: cardHeight + 'px',
-              marginBottom: cardMargin + 'px',
-
-            }
-            return (
-              <Card
-                key={index}
-                card={card}
-                index={index}
-                style={style}
-              >
-              {card}
-              </Card>
-            )
-          })
-        }
       </div>
     );
   }
--- src/components/FoldCard.js
+++ src/components/FoldCard.js
@@ -6,7 +6,7 @@ class FoldCard extends React.Component {
 		this.state = {
 			status: props.status || true,
 			distance: props.distance || 100,
-			top: props.tops || 50,
+			top: props.top || 50,
 			touchStartY: 0,
 			touchMoveY: 0,
 			touchEndY: 0,
@@ -19,6 +19,12 @@ class FoldCard extends React.Component {
 		document.addEventListener('touchend', this.touchend)
 
 	}
+
+	touchstart = (e) => {
+    this.setState({
+      touchStartY: e.targetTouches[0].pageY
+    })
+  }
   touchmove = (e) => {
     const touchMoveY = e.changedTouches[0].pageY - this.state.touchStartY
     const {status, distance} = this.state
@@ -39,6 +45,34 @@ class FoldCard extends React.Component {
       })
     }
 	}
+	touchend = (e) => {
+    const touchMoveY = e.changedTouches[0].pageY - this.state.touchStartY
+    const {status, distance} = this.state
+    if (touchMoveY > 0 && touchMoveY < distance) { // 恢复
+      console.info('huifu', touchMoveY)
+      this.setState({
+        touchMoveY: status ? 100 : 0,
+      })
+    } else if (touchMoveY >= distance) { // 展开
+      console.info('zhankai', touchMoveY)
+      this.setState({
+        touchMoveY: 100,
+        status: true
+      })
+    } else if (touchMoveY > -distance && touchMoveY < 0) { // 恢复
+      console.info('huifu', touchMoveY)
+      this.setState({
+        touchMoveY: status ? 100 : 0,
+        
+      })
+    } else if (touchMoveY <= -distance) { // 收起
+      console.info('shouqi', touchMoveY)
+      this.setState({
+        touchMoveY: 0,
+        status: false,
+      })
+    }
+  }
 	componentWillUnmount() {
     document.removeEventListener('touchstart', this.touchstart)
     document.removeEventListener('touchmove', this.touchmove)
@@ -46,16 +80,19 @@ class FoldCard extends React.Component {
     console.info('componentWillUnmount')
 	}
 	render() {
-		const {top} = this.props
-		const {touchMoveY} = this.state
+		const {touchMoveY, top, status} = this.state
 		return (
 			<div className="fold-card">
 				{this.props.children.map((child, index) => {
 					const style={
 						top: -index*(top - touchMoveY*0.5),
+						position: 'relative'
 					}
+					console.info(top)
+					console.info(touchMoveY)
+					// // console.info(child)
 					return (
-						<div style={style}>
+						<div style={style} key={index}>
 							{child}
 						</div>
 					)
